89d030ba7f128554354f61d3176944c785c83e7e
Adam-Ratajczak <99399261+Adam-Ratajczak@users.noreply.github.com>
2023-08-27 01:06:12 +0200

    React routing

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.scss";
+
+const num_sample_animes = 5
+
+function Home() {
+
+  return (
+    <>
+      <div id="content">
+        <h1>Hello, World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home'
+import Root from './Root'
+
+export {
+    Home,
+    Root
+}
