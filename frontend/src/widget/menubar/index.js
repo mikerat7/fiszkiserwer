a99452261755c9c42c144f31974f8fa4b5b585e7
mikerat7 <mikerat07@gmail.com>
2023-08-29 20:44:13 +0200

    strona3

--- frontend/src/widget/menubar/index.js
+++ frontend/src/widget/menubar/index.js
@@ -5,7 +5,8 @@ function Menubar() {
 
     return (
      <div class="menubar">
-        <a>Login</a>
+        <a href="Login">Log in</a>
+        <a href="Signup">Sign up</a>
         <a>Language</a>
         <a>Set</a>
      </div>
