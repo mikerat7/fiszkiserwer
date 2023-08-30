5d692c3210b1a4536c96f89f53bc2f7222c7ddda
mikerat7 <mikerat07@gmail.com>
2023-08-30 19:13:16 +0200

    users

--- backend/users.go
+++ backend/users.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+func hashPassword(password string) string {
+	hashed_passwd := sha256.Sum256([]byte(password))
+	return hex.EncodeToString(hashed_passwd[:])
+}
 func signup(c echo.Context) error {
 	rq := SignupUser{}
 	err := c.Bind(&rq)
@@ -14,6 +21,15 @@ func signup(c echo.Context) error {
 		// fmt.Println(err.Error())
 		return c.NoContent(http.StatusBadRequest)
 	}
+	pswh := hashPassword(rq.Password)
 
+	_, err = db.Exec("INSERT INTO user (username, password, profilepic, email) VALUES (?, ?, ?, ?);", rq.Username, pswh, "", rq.Email)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	fmt.Println(pswh)
 	return c.NoContent(http.StatusOK)
 }
